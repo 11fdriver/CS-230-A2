5bdf229f6fb779c8c622b4f0a365f5fa46da7139
unknown <tom.roach1234@gmail.com>
2020-11-16 15:39:01 +0000

    added work in progress for silk bag

---
+++ src/group_20/SilkBag.java
@@ -0,0 +1,23 @@
+public class SilkBag {
+	int numberOfTiles;
+	
+	public SilkBag() {
+		
+	}
+	
+	public boolean isEmpty() {
+		if (numberOfTiles == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public drawTile() {
+		
+	}
+	
+	public void returnTile(FloorTile) {
+		
+	}
+}
