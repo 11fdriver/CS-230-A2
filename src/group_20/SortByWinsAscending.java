3791b6a8c816fedc711944770c5a6fb0384a6c46
edward-kch <74509716+edward-kch@users.noreply.github.com>
2020-11-16 14:37:21 +0000

    Create SortByWinsAscending.java
    added SortByWinsAscending class

---
+++ src/group_20/SortByWinsAscending.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class SortByWinsAscending implements Comparator<Profile> {
+    public int compare(Profile x, Profile y) {
+        return x.getWins(Leaderboard.getBoardIDToCompare()) - y.getWins(Leaderboard.getBoardIDToCompare());
+    }
+}
