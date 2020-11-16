36017c9a56639c5983683c1a4018959822866dc0
edward-kch <74509716+edward-kch@users.noreply.github.com>
2020-11-16 14:36:52 +0000

    Create Leaderboard.java
    added Leaderboard class

---
+++ src/group_20/Leaderboard.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Leaderboard {
+	private static int boardIDToCompare;
+
+	private static ArrayList<Profile> getLeaderboard(int boardID, boolean order) {
+		return sortedProfiles(boardID, order);
+	}
+
+	private static List<Profile> getLeaderboard(int boardID, boolean order, int limit) {
+		return (sortedProfiles(boardID, order).subList(0, limit));
+	}
+
+	private static List<Profile> getLeaderboard(int boardID, boolean order, int lower, int upper) {
+		return (sortedProfiles(boardID, order).subList(lower, upper));
+	}
+	
+	private static ArrayList<Profile> sortedProfiles(int boardID, boolean order) {
+		Leaderboard.boardIDToCompare = boardIDToCompare;
+		
+		ArrayList<Profile> sortedProfiles = new ArrayList<Profile>();
+		Profile.getArrayListOfProfileInstances().clone();
+
+		if (order) {
+			Collections.sort(sortedProfiles, new SortByWinsAscending());
+		} else {
+			Collections.sort(sortedProfiles, new SortByWinsDescending());
+		}
+		
+		return sortedProfiles;
+	}
+
+	public static int getBoardIDToCompare() {
+		return boardIDToCompare;
+	}
+
+	public static void draw() {
+		//Canvas NOT yet implemented
+		for(Profile n : getLeaderboard(0, true)) {
+			System.out.print(n.getName());
+			System.out.print("\t\t");
+			System.out.println(n.getWins(0));
+			System.out.println();
+		}
+	}
+}
