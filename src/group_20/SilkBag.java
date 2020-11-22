0b07573a260ccb574057178756f38dfad09292c4
unknown <tom.roach1234@gmail.com>
2020-11-22 13:42:18 +0000

    added drawFloorTile, drawTile, and returnTile methods. added constructor

--- src/group_20/SilkBag.java
+++ src/group_20/SilkBag.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+
 public class SilkBag {
+	
 	int numberOfTiles;
 	
-	public SilkBag() {
-		
+	public SilkBag(ArrayList<Tile> tiles) {
+		numberOfTiles = (tiles.length);	
 	}
 	
 	public boolean isEmpty() {
@@ -13,11 +16,28 @@ public class SilkBag {
 		}
 	}
 	
-	public drawTile() {
+	public FloorTile drawFloorTile() {
+		x = math.random(0,2);
 		
+		if (x = 0) {
+			return Straight;
+		}
+		if (x = 1) {
+			return Corner;
+		}
+		if (x = 2) {
+			return T-Shaped;
+		}
 	}
 	
-	public void returnTile(FloorTile) {
-		
+	public Tile drawTile() {
+		return(tiles.get(0));
+		tiles.remove(0);
+		numberOfTiles = (tiles.Length);
+	}
+	
+	public void returnTile(Tile t) {
+		tiles.add (t);
+		numberOfTiles = (tiles.Length);
 	}
 }
