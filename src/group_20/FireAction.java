ac1a36c2087332729c4ed0ba31664fdf914bebc5
Finn Driver <finndriver4@gmail.com>
2020-11-23 15:03:16 +0000

    Add initial methods for FireAction

--- src/group_20/FireAction.java
+++ src/group_20/FireAction.java
@@ -1,23 +1,43 @@
 package group_20;
 
+/**
+ * Implements behaviour for when a tile is set on fire.
+ * This prevents players from moving to the affected tile, but still allows it to be shifted.
+ */
 public class FireAction extends FloorAction {
-
+	/**
+	 * Whether Action allows player movement. 
+	 */
+	private final boolean ACCEPTS_PLAYER = false;
+	
+	/**
+	 * Whether Action allows tile-shifting.
+	 */
+	private final boolean CAN_SHIFT = true;
+	
 	@Override
 	public void draw(Location loc) {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public String saveFormat() {
-		// TODO Auto-generated method stub
-		return null;
+		return "FireAction";
 	}
 
 	@Override
 	public void apply(Player p, Board b) {
-		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public boolean acceptsPlayer() {
+		return this.ACCEPTS_PLAYER;
+	}
+
+	@Override
+	public boolean canShift() {
+		return this.CAN_SHIFT;
 	}
 
 }
--- src/group_20/FloorAction.java
+++ src/group_20/FloorAction.java
@@ -1,10 +1,27 @@
 package group_20;
 
 public abstract class FloorAction extends Action {
-	protected int lifetime = 4;
+	protected final int LIFETIME = 4;
 	
+	/**
+	 * Get default lifetime of a FloorAction.
+	 * @return Turns until action should expire.
+	 */
 	public int getLifetime() {
-		return this.lifetime;
-	};
+		return this.LIFETIME;
+	}
+	
+	/**
+	 * Whether FloorAction allows a {@link Player} to go to the affected {@link FloorTile}.
+	 * @return False if doesn't allow a player to move.
+	 */
+	public abstract boolean acceptsPlayer();
+	
+	/**
+	 * Whether FloorAction allows affected {@link FloorTile} to shift.
+	 * @return False if doesn't allow a tile to shift.
+	 */
+	public abstract boolean canShift();
+
 
 }
