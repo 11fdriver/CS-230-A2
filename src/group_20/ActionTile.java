d95ffaf30d5c948653b3989eded343897535a3b1
Finn Driver <finndriver4@gmail.com>
2020-11-23 14:40:43 +0000

    Add initial methods and Javadoc for ActionTile

--- src/group_20/ActionTile.java
+++ src/group_20/ActionTile.java
@@ -1,17 +1,37 @@
 package group_20;
 
 public class ActionTile extends Tile {
-
+	/**
+	 * Action stored in ActionTile.
+	 */
+	private final Action ACTION;
+	
+	/**
+	 * User-storable and playable Tile containing an action.
+	 * @param a An action
+	 */
+	ActionTile (Action a) {
+		this.ACTION = a;
+	}
+	
+	/**
+	 * Use the action associated with an ActionTile.
+	 * @param p Player that's playing the tile
+	 * @param b Board
+	 */
+	public void play(Player p, Board b) {
+		ACTION.apply(p, b);
+	}
+	
 	@Override
 	public void draw(Location loc) {
-		// TODO Auto-generated method stub
-
+		// TODO Draw tile
+		ACTION.draw(loc);
 	}
 
 	@Override
 	public String saveFormat() {
-		// TODO Auto-generated method stub
-		return null;
+		return "ATile:" + ACTION.saveFormat();
 	}
 
 }
