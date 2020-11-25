4529bed7ae1e8988c5ab92a30a4bfe6e8bf8bda2
Finn Driver <finndriver4@gmail.com>
2020-11-25 21:50:25 +0000

    Allow easy Direction-reversing with .opposite()

--- src/group_20/Direction.java
+++ src/group_20/Direction.java
@@ -5,5 +5,19 @@ public enum Direction {
 	NORTH,
 	EAST,
 	SOUTH,
-	WEST
+	WEST;
+	
+	/**
+	 * Reverses a Direction. North -> South, East -> West.
+	 * @return Opposite direction
+	 */
+	Direction opposite() {
+		switch (this) {
+		case NORTH: return Direction.SOUTH;
+		case EAST: return Direction.WEST;
+		case SOUTH: return Direction.NORTH;
+		case WEST: return Direction.EAST;
+		default: return null; // Needed for compilation
+		}
+	}
 }
