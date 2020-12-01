61636c8e103bfb0982b715f6612e56c327e8c51d
Finn Driver <finndriver4@gmail.com>
2020-12-01 15:17:26 +0000

    Add static methods for SilkBag.{removeTile(), addTile(...)}

--- src/group_20/SilkBag.java
+++ src/group_20/SilkBag.java
@@ -1,7 +1,51 @@
 package group_20;
 
 import java.util.ArrayList;
+import java.util.Random;
 
-public class SilkBag {
+/**
+ * SilkBag is a statically-interacting class that can:
+ * <br>
+ * - Provide a random tile
+ * <br>
+ * - Re-add a tile from the {@link} Board
+ * @author fd
+ *
+ */
+public class SilkBag implements Saveable {
+	/**
+	 * Container for Tiles. This is static, no constructor needed.
+	 */
+	private static final ArrayList<Tile> TILES = new ArrayList<Tile>();
 	
+	/**
+	 * Random number generator.
+	 */
+	private static final Random RAND = new Random();
+	
+	/**
+	 * @return A randomly-chosen {@link Tile}.
+	 */
+	public static Tile removeTile() {
+		return TILES.remove(RAND.nextInt(TILES.size() - 1));
+	}
+	
+	/**
+	 * Used either when constructing the SilkBag,
+	 * <br>
+	 * or returning a tile after inserting to {@link Board}.
+	 * @param t A {@link Tile}
+	 */
+	public static void addTile(Tile t) {
+		TILES.add(t);
+	}
+
+	@Override
+	public String saveFormat() {
+		String str = "[SilkBag]";
+		for (Tile t : TILES) {
+			str += ", " + t.saveFormat();
+		}
+		return str + "[/SilkBag]";
+	}
 }
