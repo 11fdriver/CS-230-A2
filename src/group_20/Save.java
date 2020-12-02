ba221b5f9de7ec9b97d62a3a54579a50b580dcc4
Finn Driver <finndriver4@gmail.com>
2020-12-02 21:28:26 +0000

    Revert "Remove old saving/loading classes, they will be completely replaced"
    This reverts commit 093f96ff12efe118349008980fef2f38b5c9ded8.

---
+++ src/group_20/Save.java
@@ -0,0 +1,13 @@
+abstract public class Save extends IO {
+    public abstract void save();
+
+    public abstract String toString();
+        /*
+        will have the board data parsed into it and convert it to one big string
+        that will then be passed back into the save method (of its respective subclass)
+         and be committed to a file of the filename that was inherited from IO().
+         saveProfile and saveGame take in different data and will need to perform
+         different operations to do a successful toString() so it will override in
+         each class
+         */
+}
